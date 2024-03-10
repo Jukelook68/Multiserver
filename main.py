f9b55da73eccaa61d90371d04a321aa42806ffbf
Jukelook68 <97340051+Jukelook68@users.noreply.github.com>
2024-03-10 12:49:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import asyncio
+from Threading import Thread
+import git
+import DeployManager
+
+with open('config.json') as f:
+        data = json.load(f)
+        for x in data[0]:
+            name=x
+            link=x[0]
+            branch=x[1]
+            rcom=x[2]
+            
+            gitget(link, branch, name)
+            importlib.devalidatecache()
+            project = importlib.import_module("programs/"+name)
+            Thread(target = run).start()
+
+        autotime=data[1][0]
+        DeployManager.AutoDeploy.run()
+
+
