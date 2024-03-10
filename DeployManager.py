15a43e26ad0abb3e97fa3399bb783b2112d518be
Jukelook68 <97340051+Jukelook68@users.noreply.github.com>
2024-03-10 11:47:51 +0000

    Update DeployManager.py

--- DeployManager.py
+++ DeployManager.py
@@ -21,21 +21,22 @@ class AutoRedeploy():
         while auto:
             with open('config.json') as f:
                 data = json.load(f)
-                    for x in data[]:
-                        name=x
-                        link=x[0]
-                        branch=x[1]
-                        rcom=x[2]
+                for x in data[]:
+                    name=x
+                    link=x[0]
+                    branch=x[1]
+                    rcom=x[2]
 
-                        curver = open("projects/"+name+"/multiserver.txt")
+                    curver = open("projects/"+name+"/multiserver.txt")
                 
-                        repo = git.repo(link, branch)
-                        sha = str(repo.head.object.hexsha)
-                        if sha != curver:
-                            getgit(link, branch, name)
-        sleep(time)
+                    repo = git.repo(link, branch)
+                    sha = str(repo.head.object.hexsha)
+                    if sha != curver:
+                        getgit(link, branch, name)
+                f.close()
+            sleep(time)
     """ TODO
-        - Define curver and auto
-        - auto branch and dir
+        - Define auto
         - catch/disable
+        - sleep
     """
