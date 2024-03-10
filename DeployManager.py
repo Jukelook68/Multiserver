7cef88002d7a7fb1de4935d7ed8accc2aea57d59
Jukelook68 <97340051+Jukelook68@users.noreply.github.com>
2024-03-10 11:23:22 +0000

    Update DeployManager.py

--- DeployManager.py
+++ DeployManager.py
@@ -6,30 +6,30 @@ import json
 class CloneProgress(RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
         if message:
-            print(message)
-
-getconfig()
-    with open('strings.json') as f:
-        data = json.load(f)
-        for x in data[]:
-            name=x
-            link=x[0]
-            rcom=x[1]
-            branch[2]
+            print(message) 
 
 getgit(gitlink, branch, dir):
     print('Cloning into %s' % git_root)
     git.Repo.clone_from(gitlink, dir, 
             branch=branch, progress=CloneProgress())
 
-class AutoRedeploy(links):
-    run():
+class AutoRedeploy():
+    def run():
         while auto:
-            for x in len(links):
-                repo = git.repo(links[x])
-                sha = repo.head.object.hexsha
-                if sha != curver:
-                    getgit(link[x])
+            with open('strings.json') as f:
+                data = json.load(f)
+                    for x in data[]:
+                        name=x
+                        link=x[0]
+                        branch=x[1]
+                        rcom=x[2]
+
+                        curver = open(name+"/version.txt"
+                
+                        repo = git.repo(link, branch)
+                        sha = repo.head.object.hexsha
+                        if sha != curver:
+                            getgit()
     """ TODO
         - Define curver and auto
         - auto branch and dir
