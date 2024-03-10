c04f89bbd7847d61a74c398461107e3e3c9788cb
Jukelook68 <97340051+Jukelook68@users.noreply.github.com>
2024-03-10 11:12:10 +0000

    Update DeployManager.py

--- DeployManager.py
+++ DeployManager.py
@@ -1,11 +1,22 @@
 import time
 import git  # pip install gitpython
 from git import RemoteProgress
+import json
 
 class CloneProgress(RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
         if message:
             print(message)
+
+getconfig()
+    with open('strings.json') as f:
+        data = json.load(f)
+        for x in data[]:
+            name=x
+            link=x[0]
+            rcom=x[1]
+            branch[2]
+
 getgit(gitlink, branch, dir):
     print('Cloning into %s' % git_root)
     git.Repo.clone_from(gitlink, dir, 
